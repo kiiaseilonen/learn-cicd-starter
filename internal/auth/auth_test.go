dad852c054ac75e198cc0454b1e7810116521379
kiiaseilonen <kiia.seilonen@hotmail.com>
2024-08-09 11:40:43 +0300

    testing if broken code pass

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,15 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey my_test_key")
+	got, err := GetAPIKey(headers)
+	if got != "my_test_key" || != nil {
+		t.Errorf("GetAPIKey() = %s, %v; want %s, nil", got, err, "my_test_key")
+	}
+}
